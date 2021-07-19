1a31e31cec800cbbd8d6e8775b75e0412e0f1975
Igor Vospukov <igorvospukov78@gmail.com>
2021-07-19 14:11:30 +0300

    taskjs1_2

---
+++ taskJS1_2/anketaOper.js
@@ -0,0 +1,29 @@
+let surname = prompt('ваша Фамилия');
+let name = prompt('ваше Имя');
+let surSurname = prompt('ваше Отчество');
+let age = prompt('ваш Возраст');
+let gender = confirm('ваш пол мужской?');
+
+gender == true && age < 65
+  ? alert(
+      'ваше ФИО: ' +
+        surname +
+        ' ' +
+        name +
+        ' ' +
+        surSurname +
+        '\n' +
+        'ваш возраст в годах: ' +
+        age +
+        '\n' +
+        'ваш возраст в днях: ' +
+        age * 365 +
+        '\n' +
+        'через пять лет вам будет: ' +
+        (+age + 5) +
+        '\n' +
+        'ваш пол: мужской' +
+        '\n' +
+        'вы на пенсии: нет'
+    )
+  : 'see you later';
